a3a48c9be84d8d16a2b28fece70267c95ec2a4ad
an-agha <anaghanisakar@gmail.com>
2022-03-16 14:44:36 +0530

    nav bar

---
+++ script.js
@@ -0,0 +1,8 @@
+function showItem(){
+    var para = document.getElementById("nav-show");
+    para.classList.toggle("nav-class");
+}
+
+function hide(){
+    document.getElementById("nav-show").style.display='none';
+}
